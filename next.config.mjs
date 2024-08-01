96eb11ea634a6d847800f666deb49cf8e551116b
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-07-31 22:00:45 -0400

    added pwa configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
 const nextConfig = {
+    pwa:{
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
